f1466b7b03c8004a38aee91e4153a55ff3eee6e2
Kevin Levitz <tug97195@temple.edu>
2020-09-15 11:15:36 -0400

    Created text change function for onClick

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                TextView textView = (TextView)findViewById(R.id.textView);
+                textView.setText("You have clicked the button");
+            }
+
+        });
     }
 
 
